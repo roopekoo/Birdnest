062794238f5e1e324e542f1dad2330be81883c9e
Roope Korkee <roope.korkee@hotmail.com>
2022-12-19 20:45:32 +0200

    Add extractInfo function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import fetchUrl from './utils/dataFetch.js'
+import extractInfo from './utils/xmlParser.js'
 
 // Drone location data fetch url
 const url = "https://assignments.reaktor.com/birdnest/drones";
@@ -9,8 +10,9 @@ const fetchInterval = setInterval(fetchLoop, 2000);
 async function fetchLoop() {
   //Fetch data
   const xml = await fetchUrl(url);
-  console.log(xml) //Temporal check that the fetching works; Spams the console!!!
   //Extract timestamp, smallest distance and list of violators
+  const violatorInfo = extractInfo(xml);
+  console.log(violatorInfo) //Temporal check that the extractInfo works; Spams the console!!!
   //Send signal to broadcast nearest distance and list of violators
 }
 
---
+++ utils/xmlParser.js
@@ -0,0 +1,48 @@
+import DomParser from 'dom-parser';
+
+// Nest/sensor location
+const NEST_X = 250000;
+const NEST_Y = 250000;
+const RADIUS = 100000;
+
+/**
+ * Parse information from xml, such timestamp, smallest distance and list of violators
+ * @param {String} xml 
+ * @returns {Object} objecvt containing timestamp, showtest distance to the nest and list of violators
+ */
+export default function extractInfo(xml) {
+    var minDist = null;
+    var violators = []
+
+    const parser = new DomParser();
+    const xmlDoc = parser.parseFromString(xml, "text/xml");
+    const timeStamp = xmlDoc.getElementsByTagName("capture")[0].attributes[0].value;
+
+    var drones = xmlDoc.getElementsByTagName("drone")
+
+    drones.forEach(drone => {
+        const X = parseFloat(drone.getElementsByTagName("positionX")[0].innerHTML);
+        const Y = parseFloat(drone.getElementsByTagName("positionY")[0].innerHTML);
+
+        const dist = getDistance(X, Y);
+        if (minDist == null || dist < minDist) {
+            minDist = dist;
+        }
+        if (dist < RADIUS) {
+            const serial = drone.getElementsByTagName("serialNumber")[0].innerHTML;
+            violators.push({ serial: serial, distance: dist });
+        }
+    });
+
+    return { timeStamp: timeStamp, minDist: minDist, violators: violators };
+}
+
+/**
+ * Computes Euclidean distance between the drone and the nest
+ * @param {Number} X x-coordinate
+ * @param {Number} Y y-coordinate
+ * @returns {Number} Euclidean distance from the NEST
+ */
+function getDistance(X, Y) {
+    return Math.sqrt((X - NEST_X) ** 2 + (Y - NEST_Y) ** 2)
+}
