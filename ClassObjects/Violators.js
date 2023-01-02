c078f3dd4422cbf55848071cd21ecd4cbe885ec1
Roope Korkee <roope.korkee@hotmail.com>
2023-01-02 18:05:48 +0200

    Fetch only if violator does not exist in the list

--- ClassObjects/Violators.js
+++ ClassObjects/Violators.js
@@ -2,25 +2,31 @@ export default class Violators {
   violatorList = [];
   /**
    * Check if the violator is on the list
-   * @param {String} serialNumber
+   * @param {String} pilotID
    * @returns {Number} index where the user is in the list, -1 if does not exist
    */
-  findViolator (serialNumber) {
-    return this.violatorList.findIndex(e => e.pilotId === serialNumber);
+  findByPilotID(pilotId) {
+    return this.violatorList.findIndex(e => e.pilotId === pilotId);
+  }
+  /**
+   * Check if the violator is on the list
+   * @param {String} droneID
+   * @returns {Number} index where the user is in the list, -1 if does not exist
+   */
+  findByDroneID(droneId) {
+    return this.violatorList.findIndex(e => e.droneId === droneId);
   }
   /**
    * Check if the violator is closer to the nest than before and change minDist value if necessary
    * @param {Object} violatorInfo contains drone serial and current distance to the nest
    * @returns true if the drone is closer to the nest than before; false otherwise
    */
-  checkMinDist(serial,minDist)
-  {
-    const id = this.findViolator(serial);
+  checkMinDist(serial, minDist) {
+    const id = this.findByPilotID(serial);
     if (id !== -1) {
-      let violator=this.violatorList[id];
-      if(minDist <violator.minDist)
-      {
-        violator.minDist=minDist;
+      let violator = this.violatorList[id];
+      if (minDist < violator.minDist) {
+        violator.minDist = minDist;
         return true;
       }
     }
@@ -31,8 +37,8 @@ export default class Violators {
    * Add violator to the list
    * @param {Object} userJSON userData
    */
-  addViolator (userJSON) {
-    const id = this.findViolator(userJSON.pilotId);
+  addViolator(userJSON) {
+    const id = this.findByPilotID(userJSON.pilotId);
     if (id === -1) {
       this.violatorList.push(userJSON);
     }
@@ -42,8 +48,8 @@ export default class Violators {
    * Remove the violator from the list
    * @param {Object} userJSON userData
    */
-  removeViolator (serial) {
-    const id = this.findViolator(serial);
+  removeViolator(serial) {
+    const id = this.findByPilotID(serial);
     if (id !== -1) {
       this.violatorList.splice(id, 1);
     }
--- Handlers/ViolatorHandler.js
+++ Handlers/ViolatorHandler.js
@@ -13,47 +13,52 @@ export default class ViolatorHandler {
   timer = new Timer(this);
 
   /**
-	 * Set WebHandler class instance
-	 * @param {WebHandler} webInstance
-	 */
+   * Set WebHandler class instance
+   * @param {WebHandler} webInstance
+   */
   setWebInstance(webInstance) {
     this.webHandlerInstance = webInstance;
   }
 
   /**
-	 * Get the violator's info and save it to the Violator's class
-	 * @param {Object} violatorInfo
-	 */
+   * Get the violator's info and save it to the Violator's class
+   * @param {Object} violatorInfo
+   */
   handleViolators(violatorInfo) {
     // save every violator to the Violators class
     violatorInfo.violators.forEach(async violator => {
-      // Fetch violator information
-      const userData = await fetchUrl(this.url + violator.serial);
-      const userJSON = JSON.parse(userData);
-      userJSON.minDist = violator.distance;
-      const serial = userJSON.pilotId;
-      if (this.violators_class.findViolator(serial) !== -1) {
-        this.timer.resetTimer(serial);
-        if (this.violators_class.checkMinDist(serial, userJSON.minDist)) {
-          this.webHandlerInstance.updateDist({ serial: serial, distance: userJSON.minDist });
+      const violatorInd = this.violators_class.findByDroneID(violator.serial);
+      if (violatorInd !== -1) {
+        const pilotId = this.violators_class.violatorList[violatorInd].pilotId;
+        this.timer.resetTimer(pilotId);
+        if (this.violators_class.checkMinDist(pilotId, violator.distance)) {
+          this.webHandlerInstance.updateDist({ serial: pilotId, distance: violator.distance });
         }
       } else {
-        this.timer.addTimer(serial);
+        // Fetch new violator information
+        const userData = await fetchUrl(this.url + violator.serial);
+
+        const userJSON = JSON.parse(userData);
+        userJSON.droneId = violator.serial;
+        userJSON.minDist = violator.distance;
+        const pilotId = userJSON.pilotId;
+
+        this.timer.addTimer(pilotId);
         this.notifyOnAdd(userJSON);
       }
     });
     this.webHandlerInstance.updateDist({ serial: '', distance: violatorInfo.minDist });
   }
   /**
-	 * Add violator to Violators class and send message to webHandler to add an entry
-	 */
+   * Add violator to Violators class and send message to webHandler to add an entry
+   */
   notifyOnAdd(userJSON) {
     this.violators_class.addViolator(userJSON);
     this.webHandlerInstance.addUser(userJSON);
   }
   /**
-	 * Delete violator from Violators class and send message to webHandler to remove an entry
-	 */
+   * Delete violator from Violators class and send message to webHandler to remove an entry
+   */
   notifyOnDelete(serial) {
     this.violators_class.removeViolator(serial);
     this.webHandlerInstance.deleteUser(serial);
